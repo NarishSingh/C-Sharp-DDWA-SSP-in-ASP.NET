a869cae9753e4836d1791b0962d45c5244cbbadd
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-10 14:49:38 -0500

    Adding Ivalidatable SSV lesson proj

---
+++ IValidatableObjSSV/IValidatableObjSSV/Controllers/HomeController.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using IValidatableObjSSV.Models;
+
+namespace IValidatableObjSSV.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View(new ApptRequest());
+        }
+
+        [HttpPost]
+        public ActionResult Index(ApptRequest model)
+        {
+            if (ModelState.IsValid)
+            {
+                return View("Confirmation", model);
+            }
+            else
+            {
+                return View("Index", model);
+            }
+        }
+    }
+}
---
+++ IValidatableObjSSV/IValidatableObjSSV/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="IValidatableObjSSV.MvcApplication" Language="C#" %>
