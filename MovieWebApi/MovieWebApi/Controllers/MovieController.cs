b6032921aa35e491c2afb63196657b555b42aec2
Narish Singh <62305841+NarishSingh@users.noreply.github.com>
2021-02-15 12:40:12 -0500

    POST movie request created

--- MovieWebApi/MovieWebApi/Controllers/MovieController.cs
+++ MovieWebApi/MovieWebApi/Controllers/MovieController.cs
@@ -1,4 +1,5 @@
 ﻿using System.Web.Http;
+using System.Web.Http.Results;
 using MovieWebApi.Models;
 
 namespace MovieWebApi.Controllers
@@ -16,6 +17,7 @@ namespace MovieWebApi.Controllers
             return Ok(MovieRepoStub.ReadAll());
         }
 
+        /*
         /// <summary>
         /// Read by id API request using Query String
         /// </summary>
@@ -35,6 +37,53 @@ namespace MovieWebApi.Controllers
             {
                 return Ok(movie);
             }
+        }
+        */
+
+        /// <summary>
+        /// Read by id API request using URL path
+        /// </summary>
+        /// <param name="id">int for an id in stub repo</param>
+        /// <returns>200 OK with the JSON data with movie corresponding to id, 404 if invalid id</returns>
+        [Route("movies/get/{id}")]
+        [AcceptVerbs("GET")]
+        public IHttpActionResult Get(int id)
+        {
+            Movie movie = MovieRepoStub.ReadById(id);
+
+            if (movie == null)
+            {
+                return NotFound();
+            }
+            else
+            {
+                return Ok(movie);
+            }
+        }
+
+        /// <summary>
+        /// POST a new movie record
+        /// </summary>
+        /// <param name="req">CreateMovieRequest with Title and Rating in body</param>
+        /// <returns>201 Created with a GET to the new movie record, 400 Bad Request if invalid</returns>
+        [Route("movies/add")]
+        [AcceptVerbs("POST")]
+        public IHttpActionResult Add(CreateMovieRequest req)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest(ModelState);
+            }
+
+            Movie m = new Movie
+            {
+                Title = req.Title,
+                Rating = req.Rating
+            };
+            
+            MovieRepoStub.Create(m);
+
+            return Created($"movies/get/{m.Id}", m);
         }
     }
 }
---
+++ MovieWebApi/MovieWebApi/Models/CreateMovieRequest.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace MovieWebApi.Models
+{
+    public class CreateMovieRequest
+    {
+        [Required] public string Title { get; set; }
+        [Required] public string Rating { get; set; }
+    }
+}
